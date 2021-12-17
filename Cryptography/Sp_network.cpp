18da085b8981a21a8f6fbdf11f18b1b0b8d423eb
Richie Cahill <richie@tmmworkshop.com>
2021-12-16 20:26:59 -0500

    probably working sp network to encrypt 8 bit data

--- Cryptography/Sp_network.cpp
+++ Cryptography/Sp_network.cpp
@@ -1,4 +1,5 @@
 /*
+Resources i use to make this
 https://www.youtube.com/watch?v=DLjzI5dX8jc
 https://www.youtube.com/watch?v=7mwgA9XFIEQ
 */
@@ -32,7 +33,7 @@ int S_Box_1(int input) {
 }
 
 int S_Box_2(int input) {
-  if (input > 0)
+  if (input > 15)
   return -1;
   map<int, int> mp ={
     { 0,6 },
@@ -55,28 +56,55 @@ int S_Box_2(int input) {
   return mp[input];
 }
 
-int permutation_box(uint8_t input){
-  /*   1010 1010
+int P_box_1(int input){
+  /*   0111 0010
      0 abcd efgh
      1 fgha bcde
      2 hafg debc
      3 hade fgbc
      4 hdae fbgc 
      5 hdae fbcg
-       0011 0011
+       0100 0111
   */
 
 input = input >> 3 | input << 5;
 input = ((input & 0xcc)>>2) | ((input & 0x33)<<2);
 input = (input & 0xc3) | ((input & 0x30)>>2) | ((input & 0xc)<<2);
 input = (input & 0x99) | ((input & 0x44)>>1) | ((input & 0x22)<<1);
-input = (input & 0x99) | ((input & 0x2)>>1) | ((input & 0x1)<<1);
+input = (input & 0xFC) | ((input & 0x2)>>1) | ((input & 0x1)<<1);
 
 return input;  
 }
 
+int round(int data){
+  for (int i = 0; i <= 5; i++){
+    int a, b, c;
+    a = data & 0xf;
+    b = data >> 4;
+    c = S_Box_1(a) | (S_Box_2(b) << 4);
+    data = P_box_1(c);
+  }
+  return data;
+}
+
+int encryption(int data,int key){
+  int keya , keyb ;
+  keya = key & 0xff;
+  keyb = key >> 8;
+  data = round(data);
+  data = data ^ keya;
+  data = round(data);
+  data = data ^ keyb;
+  data = round(data);
+
+
+  return data;
+}
 
 int main(int argc, char const *argv[]) {
+  int test = 300;
+  test = encryption(test,0xED6E);
+  cout << test << endl;
 
   return 0;
 }
