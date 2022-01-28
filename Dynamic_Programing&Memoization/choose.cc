7fe0b3ca4779a4404ad921dc85622d35d63c7296
Richie Cahill <richie@tmmworkshop.com>
2022-01-28 17:22:46 -0500

    added the abiltiy to read and wright the dactoreal table

--- Dynamic_Programing&Memoization/choose.cc
+++ Dynamic_Programing&Memoization/choose.cc
@@ -1,21 +1,56 @@
 //Alice Houston
 #include<iostream>
 #include<vector>
+#include <fstream>
+#include <sys/stat.h>
 
 using namespace std;
-
 //choose(n,r) = n!/(r! (n-r)!) = 52! / (6! 46!)
 
+vector<double> factmemo{1};
+void Reader(){
+	struct stat buffer;
+	if(stat("./facttable.txt", &buffer) != 0)
+		return;
+  // Create a text string, which is used to output the text file
+  double myText;
+
+  // Read from the text file
+  ifstream MyReadFile("./facttable.txt");
+  factmemo.pop_back();
+  while (MyReadFile >> myText) {
+    if (factmemo.size() == factmemo.capacity())
+  		factmemo.reserve(factmemo.size()*1.50);
+  	factmemo.push_back(myText);
+  }
+  // Close the file
+  MyReadFile.close(); 
+}
+
+void Writer(){
+  // Create and open a text file
+  ofstream Factfile("./facttable.txt");
+  
+  // Write to the file
+  for (size_t i = 0; i < factmemo.size(); i++){
+    if (factmemo[i] == 0)
+      break;
+    Factfile << factmemo[i] << endl;
+  }
+
+  // Close the file
+  Factfile.close();
+}
+
 double fact(int32_t n) {
-	static vector<double> memo{1,1,2,6,24,120,720,5040,40320,362880,3628800};
-	if (n < memo.size())
-		return memo[n];
-	for (uint32_t i = memo.size(); i <= n; i++) {
-	if (memo.size() == memo.capacity())
-		memo.reserve(memo.size()*1.50);
-		memo.push_back(memo[i-1] * i);
+	if (n < factmemo.size())
+		return factmemo[n];
+	for (uint32_t i = factmemo.size(); i <= n; i++) {
+	if (factmemo.size() == factmemo.capacity())
+		factmemo.reserve(factmemo.size()*1.50);
+		factmemo.push_back(factmemo[i-1] * i);
 	}
-	return memo[n];
+	return factmemo[n];
 }
 
 double choose(int n, int r) {
@@ -29,10 +64,13 @@ double choose(int n, int r) {
 }
 
 int main(){
+
+	Reader();
 	cout << fact(20) << '\n';
 	cout << fact(170) << '\n';
 	cout << fact(171) << '\n';
 	cout << choose(52, 6) << '\n';
-	
+	Writer();
+
 	return 0;
 }
