151e83d1f34d679d9ae16ed024b359cc8e55c9eb
Richie Cahill <richie@tmmworkshop.com>
2022-01-28 21:42:00 -0500

    added resources i used to the top

--- Dynamic_Programing&Memoization/choose.cc
+++ Dynamic_Programing&Memoization/choose.cc
@@ -2,6 +2,7 @@
 //Alice Houston
 //https://stackoverflow.com/questions/12774207/fastest-way-to-check-if-a-file-exist-using-standard-c-c11-14-17-c
 //https://www.w3schools.com/cpp/cpp_files.asp
+//https://linux.die.net/man/2/stat
 
 #include<iostream>
 #include<vector>
