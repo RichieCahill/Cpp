8f95e68afa5bb3ead4f30618603136d0e27c5764
Richie Cahill <richie@tmmworkshop.com>
2022-02-22 14:20:14 -0500

    got buildmask work on avx registers and added themasks for 11 to 31

--- AVX/avx2_primelist.cc
+++ AVX/avx2_primelist.cc
@@ -42,19 +42,30 @@ __m256i avx256_ls_test (__m256i n, int64_t s){
 	return n;
 }
 
-// build a mask where every kth bit is zero starting from right
-uint64_t buildmask(uint32_t k) {
-	uint64_t t = 0;
-	for (uint32_t i = 0; i < 64; i += k)
-		t = t | (1ULL << i);
+// build a mask where every kth bit is one starting from right
+__m256i buildmask(uint32_t k) {
+	__m256i t = _mm256_setzero_si256();
+	const __m256i mask1 = _mm256_set_epi64x(0ULL,0ULL,0ULL,1ULL);
+	for (uint32_t i = 0; i < 256; i += k){
+		t = avx256_ls_test(t, k);
+		t = _mm256_or_si256(t, mask1);
+	}
 	return t;
 }
 
 
 int main() {
-	const __m256i mask3 = _mm256_set_epi64x(0x9249249249249249,0x2492492492492492,0x4924924924924924,0x9249249249249249);
-	const __m256i mask5 = _mm256_set_epi64x(0x8421084210842108,0x4210842108421084,0x2108421084210842,0x1084210842108421);
+	__m256i mask31 = buildmask(31);
+	__m256i mask29 = buildmask(29);
+	__m256i mask23 = buildmask(23);
+	__m256i mask19 = buildmask(19);
+	__m256i mask17 = buildmask(17);
+	const __m256i mask13 = _mm256_set_epi64x(0x0080040020010008,0x0040020010008004,0x0020010008004002,0x0010008004002001);
+	const __m256i mask11 = _mm256_set_epi64x(0x2004008010020040,0x0801002004008010,0x0200400801002004,0x0080100200400801);
 	const __m256i mask7 = _mm256_set_epi64x(0x1020408102040810,0x2040810204081020,0x4081020408102040,0x8102040810204081);
+	const __m256i mask5 = _mm256_set_epi64x(0x8421084210842108,0x4210842108421084,0x2108421084210842,0x1084210842108421);
+	const __m256i mask3 = _mm256_set_epi64x(0x9249249249249249,0x2492492492492492,0x4924924924924924,0x9249249249249249);
+
 
 	constexpr uint64_t n = 3500;
 	constexpr int size = (n+511)/512;
