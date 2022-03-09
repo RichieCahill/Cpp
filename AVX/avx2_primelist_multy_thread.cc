02ab6604e8ac98573e0797053c4f01986581cb36
Richie Cahill <richie@tmmworkshop.com>
2022-03-09 17:05:24 -0500

    converted avx2_primelist_multy_thread.cc to my libreary

--- AVX/avx2_primelist_multy_thread.cc
+++ AVX/avx2_primelist_multy_thread.cc
@@ -25,54 +25,17 @@ tiem to beat to 10,000,000,000 12s
 #include <string>
 #include <vector>
 #include <cmath>
+#include"AVX-bit-manipulation.h"
 
 using namespace std;
 
-//couts an AVX register right to left
-void avxout(__m256i a){
-	cout << hex << _mm256_extract_epi64(a, 0) << endl;
-	cout << hex << _mm256_extract_epi64(a, 1) << endl;
-	cout << hex << _mm256_extract_epi64(a, 2) << endl;
-	cout << hex << _mm256_extract_epi64(a, 3) << endl;
-	cout << endl;
-}
-
-//Leftshifts a avx256 register
-__m256i avx256_ls_test (__m256i n, int64_t s){
-	if (s==0)
-		return n;
-	__m256i temp;
-	__m128i t128_0, t128_1;
-	uint64_t t64_0, t64_1;
-	//pulls the avx register apart and left shits stor the out put in a __m128i
-	t128_0 =	_mm256_extracti128_si256(n, 0);
-	t64_0 = _mm_extract_epi64(t128_0, 0);
-	t64_0 = t64_0>> (64-s);
-	t64_1 = _mm_extract_epi64(t128_0, 1);
-	t64_1 = t64_1>> (64-s);
-	t128_0 = _mm_set_epi64x(t64_0, 0X0000000000000000);
-
-	//pulls the avx register apart and left shits stor the out put in a __m128i
-	t128_1 = _mm256_extracti128_si256(n, 1);
-	t64_0 = _mm_extract_epi64(t128_1, 0);
-	t64_0 = t64_0>> (64-s);
-	t128_1	= _mm_set_epi64x(t64_0, t64_1);
-
-	//combins the 2 __m128i into a __m256i
-	temp = _mm256_set_m128i(t128_1, t128_0);
-
-	// left shit the 4 64 bit ins in n then or with temp
-	n = n << s;
-	n = _mm256_or_si256(n, temp);
-	return n;
-}
 
 // build a mask where every kth bit is one starting from right
 __m256i buildmask(uint32_t k) {
 	__m256i t = _mm256_setzero_si256();
 	const __m256i mask1 = _mm256_set_epi64x(0ULL,0ULL,0ULL,1ULL);
 	for (uint32_t i = 0; i < 256; i += k){
-		t = avx256_ls_test(t, k);
+		t = _mm256_lls_mm256(t, k);
 		t = _mm256_or_si256(t, mask1);
 	}
 	return t;
@@ -110,32 +73,32 @@ void list_generator(uint64_t s, uint64_t e, __m256i* prime){
 		//patern 1 0 2
 		// cout << 2-(i%3) << endl;
 
-		temp = _mm256_or_si256(temp, avx256_ls_test(mask3,2-(i%3)));
-		// temp = _mm256_or_si256(temp, avx256_ls_test(mask3, (5-(i%3))%3));
+		temp = _mm256_or_si256(temp, _mm256_lls_mm256(mask3,2-(i%3)));
+		// temp = _mm256_or_si256(temp, _mm256_lls_mm256(mask3, (5-(i%3))%3));
 
 		// masks multiple of 5
 		//patern 2 1 0 4 3
-		temp = _mm256_or_si256(temp, avx256_ls_test(mask5,(8-(i%5))%5));
+		temp = _mm256_or_si256(temp, _mm256_lls_mm256(mask5,(8-(i%5))%5));
 
 		// masks multiple of 7
 		//patern 3 6 2 5 1 4 0
 		// cout << (11*i-(i%7))%7 << endl;
-		temp = _mm256_or_si256(temp, avx256_ls_test(mask7,(11*i-(i%7))%7));
+		temp = _mm256_or_si256(temp, _mm256_lls_mm256(mask7,(11*i-(i%7))%7));
 
 
 		// masks multiple of 11
 		//pttern 4 1 9 6 3 0 8 5 2 10 7
 		// int n = 11, k =5;
-		temp = _mm256_or_si256(temp, avx256_ls_test(mask11,((9*i-4)-(i%11))%11));
+		temp = _mm256_or_si256(temp, _mm256_lls_mm256(mask11,((9*i-4)-(i%11))%11));
 
 		// masks multiple of 11
 		//pttern 5 9 0 4 8 12 3 7 11 2 6 10 1
-		temp = _mm256_or_si256(temp, avx256_ls_test(mask13,((5*i+1)-(i%13))%13));
+		temp = _mm256_or_si256(temp, _mm256_lls_mm256(mask13,((5*i+1)-(i%13))%13));
 
 		//Outputs the avx register to disk in raw
 		prime[i-1]=temp;
 		// Factfile.write ((char*)&temp, sizeof (temp));
-		// avxout(temp);
+		avxout(temp);
 	}
 	// Factfile.close();
 }
