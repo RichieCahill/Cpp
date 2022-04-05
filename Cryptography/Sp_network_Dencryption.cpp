48dc86bbae651fe666e8ea75fba46d3082694524
Richie Cahill <richie@tmmworkshop.com>
2022-04-04 22:38:57 -0400

    Updated Sp_network Encryption Dencryption added type def and modernd typs convrted the maps to list and genrly optomised evreything

--- Cryptography/Sp_network_Dencryption.cpp
+++ Cryptography/Sp_network_Dencryption.cpp
@@ -4,124 +4,61 @@ https://www.youtube.com/watch?v=DLjzI5dX8jc
 https://www.youtube.com/watch?v=7mwgA9XFIEQ
 */
 #include <iostream>
-#include <map>
 
 using namespace std;
 
-uint32_t S_Box_1(uint32_t input) {
-  if (input > 15)
-  return -1;
-  map<uint32_t, uint32_t> mp ={
-    { 8,0 },
-    { 2,1 },
-    { 4,2 },
-    { 11,3 },
-    { 6,4 },
-    { 14,5 },
-    { 7,6 },
-    { 1,7 },
-    { 3,8 },
-    { 13,9 },
-    { 9,10 },
-    { 12,11 },
-    { 15,12 },
-    { 5,13 },
-    { 0,14 },
-    { 10,15 },
-  };
-  return mp[input];
+typedef uint64_t u64;
+typedef uint32_t u32;
+typedef uint16_t u16;
+typedef uint8_t u8;
+
+typedef int64_t i64;
+typedef int32_t i32;
+typedef int16_t i16;
+typedef int8_t i8;
+
+
+u32 P_box_1(u32 input){
+	/*
+		5 gchd eabf
+		4 gchd eafb
+		3 ghcd efab
+		2 ghef cdab
+		1 efgh abcd
+		0 abcd efgh
+	*/
+
+	input = (input & 0xFC) | ((input & 0x2)>>1) | ((input & 0x1)<<1); //5
+	input = (input & 0x99) | ((input & 0x44)>>1) | ((input & 0x22)<<1); //4
+	input = (input & 0xc3) | ((input & 0x30)>>2) | ((input & 0xc)<<2);//3
+	input = (input & 0x00) | ((input & 0xcc)>>2) | ((input & 0x33)<<2); //2
+	return  (input & 0x00) | ((input & 0xF0)>>4) | ((input & 0xF)<<4);
+	// input = (input << 4) | (input >> 4);//1
 }
 
-uint32_t S_Box_2(uint32_t input) {
-  if (input > 15)
-  return -1;
-  map<uint32_t, uint32_t> mp ={
-    { 6,0 },
-    { 13,1 },
-    { 1,2 },
-    { 5,3 },
-    { 7,4 },
-    { 12,5 },
-    { 8,6 },
-    { 3,7},
-    { 2,8 },
-    { 0,9 },
-    { 14,10 },
-    { 10,11 },
-    { 15,12 },
-    { 9,13 },
-    { 11,14 },
-    { 4,15 },
-  };
-  return mp[input];
+u32 Round(u32 data){
+	constexpr int32_t SBox1 [16]{14,7,1,8,2,13,4,6,0,10,15,3,11,9,5,12};
+	constexpr int32_t SBox2 [16]{9,2,8,7,15,3,0,4,6,13,11,14,5,1,10,12};
+
+	for (u32 i = 0; i <= 10000; i++){
+		data = P_box_1(data);
+		data = SBox1[data & 0xf] | (SBox2[data >> 4] << 4);
+	}
+	return data;
 }
 
-uint32_t P_box_1(uint32_t input){
-    /*
-    5 gchd eabf
-    4 gchd eafb
-    3 ghcd efab
-    2 ghef cdab
-    1 efgh abcd
-    0 abcd efgh
-  */
-
-  input = (input & 0xFC) | ((input & 0x2)>>1) | ((input & 0x1)<<1); //5
-  input = (input & 0x99) | ((input & 0x44)>>1) | ((input & 0x22)<<1); //4
-  input = (input & 0xc3) | ((input & 0x30)>>2) | ((input & 0xc)<<2);//3
-  input = (input & 0x00) | ((input & 0xcc)>>2) | ((input & 0x33)<<2); //2
-  input = (input & 0x00) | ((input & 0xF0)>>4) | ((input & 0xF)<<4);
-  // input = (input << 4) | (input >> 4);//1
-return input;  
-}
-
-uint32_t Round(uint32_t data){
-  for (uint32_t i = 0; i <= 10000; i++){
-    uint32_t a, b, c;
-    data = P_box_1(data);
-    a = data & 0xf;
-    b = data >> 4;
-    data = S_Box_1(a) | (S_Box_2(b) << 4);
-  }
-  return data;
-}
-
-uint32_t Dencryption(uint32_t data,uint32_t key){
-  uint32_t keya , keyb ;
-  keya = key & 0xFF;
-  keyb = (key >> 8) & 0xFF;
-  data = Round(data);
-  data = data ^ keyb;
-  data = Round(data);
-  data = data ^ keya;
-  data = Round(data);
-  return data;
+u32 Dencryption(u32 data,u32 key){
+	return Round(Round(Round(data) ^ (key >> 8) & 0xFF) ^ key & 0xFF);
 }
 
 int main(int argc, char const *argv[]) {
-  uint32_t test = 218;
-  test = Dencryption(test,0xED6E);
-  cout << test << endl;
 
-  test = 241;
-  test = Dencryption(test,0xED6E);
-  cout << test << endl;
+	cout << Dencryption(218,0xED6E) << endl;
+	cout << Dencryption(241,0xED6E) << endl;
+	cout << Dencryption(12,0x6FED) << endl;
+	cout << Dencryption(239,0xED6E) << endl;
+	cout << Dencryption(188,0xED6E) << endl;
+	cout << Dencryption(63,0xED6E) << endl;
 
-  test = 12;
-  test = Dencryption(test,0x6FED);
-  cout << test << endl;
-
-  test = 239;
-  test = Dencryption(test,0xED6E);
-  cout << test << endl;
-  
-  test = 188;
-  test = Dencryption(test,0xED6E);
-  cout << test << endl;
-  
-  test = 63;
-  test = Dencryption(test,0xED6E);
-  cout << test << endl;
-
-  return 0;
+	return 0;
 }
--- Cryptography/Sp_network_Encryption.cpp
+++ Cryptography/Sp_network_Encryption.cpp
@@ -4,125 +4,58 @@ https://www.youtube.com/watch?v=DLjzI5dX8jc
 https://www.youtube.com/watch?v=7mwgA9XFIEQ
 */
 #include <iostream>
-#include <map>
 
 using namespace std;
 
-uint32_t S_Box_1(uint32_t input) {
-  if (input > 15)
-  return -1;
-  map<uint32_t, uint32_t> mp ={
-    { 0,8 },
-    { 1,2 },
-    { 2,4 },
-    { 3,11 },
-    { 4,6 },
-    { 5,14 },
-    { 6,7 },
-    { 7,1 },
-    { 8,3 },
-    { 9,13 },
-    { 10,9 },
-    { 11,12 },
-    { 12,15 },
-    { 13,5 },
-    { 14,0 },
-    { 15,10 },
-  };
-  return mp[input];
+typedef uint64_t u64;
+typedef uint32_t u32;
+typedef uint16_t u16;
+typedef uint8_t u8;
+
+typedef int64_t i64;
+typedef int32_t i32;
+typedef int16_t i16;
+typedef int8_t i8;
+
+u32 P_box_1(u32 input){
+	/*
+		0 abcd efgh
+		1 efgh abcd
+		2 ghef cdab
+		3 ghcd efab
+		4 gchd eafb
+		5 gchd eabf
+	*/
+	input = (input & 0x00) | ((input & 0xF0)>>4) | ((input & 0xF)<<4); //1
+	input = (input & 0x00) | ((input & 0xcc)>>2) | ((input & 0x33)<<2);//2
+	input = (input & 0xc3) | ((input & 0x30)>>2) | ((input & 0xc)<<2);//3
+	input = (input & 0x99) | ((input & 0x44)>>1) | ((input & 0x22)<<1);//4
+	return  (input & 0xFC) | ((input & 0x2)>>1) | ((input & 0x1)<<1);//5
 }
 
-uint32_t S_Box_2(uint32_t input) {
-  if (input > 15)
-  return -1;
-  map<uint32_t, uint32_t> mp ={
-    { 0,6 },
-    { 1,13 },
-    { 2,1 },
-    { 3,5 },
-    { 4,7 },
-    { 5,12 },
-    { 6,8 },
-    { 7,3},
-    { 8,2 },
-    { 9,0 },
-    { 10,14 },
-    { 11,10 },
-    { 12,15 },
-    { 13,9 },
-    { 14,11 },
-    { 15,4 },
-  };
-  return mp[input];
+u32 Round(u32 data){
+	constexpr int32_t SBox1 [16]{8,2,4,11,6,14,7,1,3,13,9,12,15,5,0,10};
+	constexpr int32_t SBox2 [16]{6,13,1,5,7,12,8,3,2,0,14,10,15,9,11,4};
+
+	for (u32 i = 0; i <= 10000; i++){
+		data = P_box_1(SBox1[data & 0xf] | (SBox2[data >> 4] << 4));
+	}
+	return data;
 }
 
-uint32_t P_box_1(uint32_t input){
-  /*
-    0 abcd efgh
-    1 efgh abcd
-    2 ghef cdab
-    3 ghcd efab
-    4 gchd eafb
-    5 gchd eabf
-  */
-
-  input = (input & 0x00) | ((input & 0xF0)>>4) | ((input & 0xF)<<4); //1
-  input = (input & 0x00) | ((input & 0xcc)>>2) | ((input & 0x33)<<2);//2
-  input = (input & 0xc3) | ((input & 0x30)>>2) | ((input & 0xc)<<2);//3
-  input = (input & 0x99) | ((input & 0x44)>>1) | ((input & 0x22)<<1);//4
-  input = (input & 0xFC) | ((input & 0x2)>>1) | ((input & 0x1)<<1);//5
-
-  return input;  
-}
-
-uint32_t Round(uint32_t data){
-  for (uint32_t i = 0; i <= 10000; i++){
-    uint32_t a, b, c;
-    a = data & 0xf;
-    b = data >> 4;
-    c = S_Box_1(a) | (S_Box_2(b) << 4);
-    data = P_box_1(c);
-  }
-  return data;
-}
-
-uint32_t Encryption(uint32_t data,uint32_t key){
-  uint32_t keya , keyb ;
-  keya = key & 0xFF;
-  keyb = (key >> 8) & 0xFF;
-  data = Round(data);
-  data = data ^ keya;
-  data = Round(data);
-  data = data ^ keyb;
-  data = Round(data);
-  return data;
+u32 Encryption(u32 data,u32 key){
+	return Round((Round((Round(data) ^ key & 0xFF)) ^ (key >> 8) & 0xFF));
 }
 
 int main(int argc, char const *argv[]) {
-  uint32_t test = 171;
-  test = Encryption(test,0xED6E);
-  cout << test << endl;
 
-  test = 170;
-  test = Encryption(test,0xED6E);
-  cout << test << endl;
+	cout << Encryption(171,0xED6E) << endl;
+	cout << Encryption(170,0xED6E) << endl;
+	cout << Encryption(170,0x6FED) << endl;
+	cout << Encryption(255,0xED6E) << endl;
+	cout << Encryption(0,0xED6E) << endl;
+	cout << Encryption(100,0xED6E) << endl;
 
-  test = 170;
-  test = Encryption(test,0x6FED);
-  cout << test << endl;
-
-  test = 255;
-  test = Encryption(test,0xED6E);
-  cout << test << endl;
-  
-  test = 0;
-  test = Encryption(test,0xED6E);
-  cout << test << endl;
-  
-  test = 100;
-  test = Encryption(test,0xED6E);
-  cout << test << endl;
-
-  return 0;
+	return 0;
 }
 
